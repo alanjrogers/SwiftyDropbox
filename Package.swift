6d17647143913fb1f0ccc3d39dadefd1f00a5d68
Alan Rogers <alanjrogers@me.com>
2020-07-16 13:23:21 +1000

    Really fix it.

--- Package.swift
+++ Package.swift
@@ -2,16 +2,24 @@
 ///
 /// Copyright (c) 2016 Dropbox, Inc. All rights reserved.
 ///
-
 import PackageDescription
 
 let package = Package(
     name: "SwiftyDropbox",
-    platforms: [.iOS(.v12)],
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_13)
+	],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftyDropbox",
+            targets: ["SwiftyDropbox"]),
+	],
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.2.1")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.2.1")),
     ],
-    targets: [
- 		.target(name: "SwiftyDropbox", dependencies: ["AlamoFire"])
-    ]
+	targets: [
+		.target(name: "SwiftyDropbox", dependencies: ["Alamofire"])
+	]
 )
